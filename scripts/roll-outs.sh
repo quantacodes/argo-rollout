990f23e2adba9d9443985ce0208a46da6fa51018
Saurabh Parmar <saurabh@blokumet.com>
2024-02-10 12:46:35 +0530

    change: version

--- scripts/roll-outs.sh
+++ scripts/roll-outs.sh
@@ -4,4 +4,3 @@ export KUBECONFIG=/Users/saura/Projects/k8s/do_infra/terraform-digitalocean-k8s/
 Echo "Installing Argo-Rollouts"
 kubectl create namespace argo-rollouts
 kubectl apply -n argo-rollouts -f https://raw.githubusercontent.com/argoproj/argo-rollouts/stable/manifests/install.yaml
-
